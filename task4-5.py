6698409e724786ea6c174c4481e9450598acebed
Pabloold <73492834+Pabloold@users.noreply.github.com>
2020-11-09 18:31:27 +0300

    Update task4-5.py
    Исправлена ошибка: вместо сложения, должно было быть умножение

--- task4-5.py
+++ task4-5.py
@@ -2,4 +2,4 @@ from functools import reduce
 
 my_list = [num for num in range(100, 1001) if num % 2 == 0]
 print(my_list)
-print(reduce(lambda total, amount: total + amount, my_list))
+print(reduce(lambda total, amount: total * amount, my_list))
